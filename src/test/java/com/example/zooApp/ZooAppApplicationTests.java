44bac1563e75b7c79cd153c45fa14fb8d72a1913
Aniko Magyar <mofolade@gmail.com>
2020-09-24 21:18:38 +0200

    install

---
+++ src/test/java/com/example/zooApp/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
